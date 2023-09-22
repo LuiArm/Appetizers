c7761f86099c52b090f8c04211b92e073580e518
LuiArm <Luisarmendariz24@icloud.com>
2023-09-22 12:21:26 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by luis armendariz on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
