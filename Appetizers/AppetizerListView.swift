5c10114e07bb5a8bd5bbab164284b2dda4e98252
LuiArm <Luisarmendariz24@icloud.com>
2023-09-27 22:57:08 -0600

    add errors and alert. Add progress view when loading

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,33 +8,30 @@
 import SwiftUI
 
 struct AppetizerListView: View {
-    @State private var appetizers: [Appetizer] = []
+    //Initializing state object
+    @StateObject var viewModel = AppetizerListViewModel()
+    
     
     
     var body: some View {
-        NavigationStack{
-            List(appetizers, id: \.id){ appetizer in
-                AppetizerListCell(appetizer: appetizer)
-                
-            }
-            .navigationTitle("🌮 Appetizers")
-        }
-        .onAppear{
-            getAppetizers()
-        }
-    }
-    
-    func getAppetizers() {
-        //On background thread but need to be on main so use Dispatch que main
-        NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    print(error.localizedDescription)
+        ZStack{
+            NavigationStack{
+                List(viewModel.appetizers, id: \.id){ appetizer in
+                    AppetizerListCell(appetizer: appetizer)
+                    
                 }
+                .navigationTitle("🌮 Appetizers")
             }
+            .onAppear{
+                viewModel.getAppetizers()
+            }
+            if viewModel.isLoading {
+                ProgressView()
+                    
+            }
+        }
+        .alert(item: $viewModel.alertItem){alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton )
         }
     }
 }
---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by luis armendariz on 9/27/23.
+//
+
+import Foundation
+
+
+final class AppetizerListViewModel: ObservableObject {
+    @Published  var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
+    @Published var isLoading = false
+    
+    func getAppetizers() {
+        isLoading = true
+        //On background thread but need to be on main so use Dispatch que main
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async { [self] in
+                isLoading = false
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers // still need self here to differ from the same named appetizers
+                    
+                case .failure(let error): //error passed in from network manager
+                    switch error {
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                        
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
+    }
+}
