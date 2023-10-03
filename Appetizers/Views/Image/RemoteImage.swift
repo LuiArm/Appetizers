06256bda474915919bbe2a576288f4db076168ce
LuiArm <Luisarmendariz24@icloud.com>
2023-10-02 22:19:22 -0600

    Notes and folder organization

--- Appetizers/Views/Image/RemoteImage.swift
+++ Appetizers/Views/Image/RemoteImage.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 final class ImageLoader: ObservableObject {
     
+    //Downloads image and will broadcast once it is downloaded
     @Published var image: Image? = nil
     
+    
+    //make network call
     func load(fromURLString urlString: String) {
         NetworkManager.shared.downloadImage(fromURLString: urlString) { uiImage in
             guard let uiImage = uiImage else { return }
             DispatchQueue.main.async {
+                //setting swiftui image from uikit image
                 self.image = Image(uiImage: uiImage)
             }
         }
@@ -23,6 +27,7 @@ final class ImageLoader: ObservableObject {
 
 
 struct RemoteImage: View {
+    //optional image because we may not have an image
     var image: Image?
     
     var body: some View {
@@ -34,12 +39,15 @@ struct RemoteImage: View {
 
 struct AppetizerRemoteImage: View {
     
+    // watching this image loader, and iniatilizing new image loader
     @StateObject var imageLoader = ImageLoader()
+    
+    //string passed in depending on appetizer
     let urlString: String
     
     var body: some View {
         RemoteImage(image: imageLoader.image)
-            .onAppear {
+            .onAppear { // on appear calls image loader . load to pass in url to make network call
                 imageLoader.load(fromURLString: urlString)
             }
     }
