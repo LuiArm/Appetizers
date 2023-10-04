be834890df22f9df1b11768cd39ff25e93509c2e
LuiArm <Luisarmendariz24@icloud.com>
2023-10-03 22:56:05 -0600

    Connect data to detail view

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,21 +10,31 @@ import SwiftUI
 struct AppetizerListView: View {
     //Initializing state object
     @StateObject var viewModel = AppetizerListViewModel()
-    
-    
+    @State private var isShowingDetail = false
+    @State private var selectedAppetizer: Appetizer?
     
     var body: some View {
         ZStack{
             NavigationStack{
                 List(viewModel.appetizers, id: \.id){ appetizer in
                     AppetizerListCell(appetizer: appetizer)
-                    
+                        .onTapGesture {
+                            selectedAppetizer = appetizer
+                            isShowingDetail = true
+                        }
                 }
                 .navigationTitle("🌮 Appetizers")
+                .disabled(isShowingDetail)
             }
             .onAppear{
                 viewModel.getAppetizers()
             }
+            .blur(radius: isShowingDetail ? 20 : 0)
+            
+            if isShowingDetail {
+                AppetizerDetailView(appetizer: selectedAppetizer!, isShowingDetail: $isShowingDetail)
+            }
+            
             if viewModel.isLoading {
                 ProgressView()
                     
