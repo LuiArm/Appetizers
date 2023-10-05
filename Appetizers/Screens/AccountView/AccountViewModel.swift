0e28f2458714e5039b79b0b696e6b14a301dced3
LuiArm <Luisarmendariz24@icloud.com>
2023-10-04 23:14:35 -0600

    account view model and text validation

---
+++ Appetizers/Screens/AccountView/AccountViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AccountViewModel.swift
+//  Appetizers
+//
+//  Created by luis armendariz on 10/4/23.
+//
+
+import Foundation
---
+++ Appetizers/Utilities/Extensions/String+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  String+Ext.swift
+//  Appetizers
+//
+//  Created by luis armendariz on 10/4/23.
+//
+
+import Foundation
