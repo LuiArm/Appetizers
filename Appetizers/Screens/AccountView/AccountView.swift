53fd3c3fbef2072241210c65a8f830c8639eee91
LuiArm <Luisarmendariz24@icloud.com>
2023-10-04 22:15:11 -0600

    Account view setup

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    //Give default value to not have to iniate with values
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var extraSauce = false
+    
+    
     var body: some View {
         NavigationStack{
-            Text("Account View")
-                .navigationTitle("🧾 Accounts")
+            Form {
+                Section(header: Text("Personal Info")){
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                    DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button{
+                       print("saved")
+                    }label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Extra Sauce", isOn: $extraSauce)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+               
+            }
+            .navigationTitle("🧾 Accounts")
         }
     }
 }
