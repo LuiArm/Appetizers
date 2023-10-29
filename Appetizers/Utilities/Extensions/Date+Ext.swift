3ec90f697085a6bd4719e5088d9cef31ba4d1e21
LuiArm <Luisarmendariz24@icloud.com>
2023-10-29 14:05:01 -0600

    Date picker optimization, progress view

---
+++ Appetizers/Utilities/Extensions/Date+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Ext.swift
+//  Appetizers
+//
+//  Created by luis armendariz on 10/29/23.
+//
+
+import Foundation
---
+++ Appetizers/Views/LoadingView/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Appetizers
+//
+//  Created by luis armendariz on 10/29/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
